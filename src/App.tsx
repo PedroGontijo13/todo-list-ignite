931324da8e837bd13a3cdc6ab4d228eb4fb74913
Pedro Tolentino <pedrotolentino2002@gmail.com>
2025-03-30 02:01:32 -0300

    Added state to save inputs from the users

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
+import { useState } from 'react'
 import { AddNewTaskForm } from './components/AddNewTaskForm'
 import { Header } from './components/Header'
 
 function App() {
+  const [tasks, setTasks] = useState<string[]>([])
+
+  const handleCreateNewTask = (task: string) => {
+    setTasks([...tasks, task])
+    console.log(tasks)
+  }
 
   return (
     <div>
       <Header />
-      <AddNewTaskForm />
+      <AddNewTaskForm onCreateNewTask={handleCreateNewTask} />
     </div>
   )
 }
--- src/components/AddNewTaskForm.tsx
+++ src/components/AddNewTaskForm.tsx
@@ -1,8 +1,32 @@
-export function AddNewTaskForm() {
+import { FormEvent, useState, ChangeEvent } from 'react'
+import styles from './AddNewTaskForm.module.css'
+import { PlusCircle } from 'phosphor-react'
+
+interface AddNewTaskFormProps {
+    onCreateNewTask: (task: string) => void
+}
+
+export function AddNewTaskForm({ onCreateNewTask }: AddNewTaskFormProps) {
+    const [newTask, setNewTask] = useState<string>('')
+
+    const handleNewTaskChange = (event: ChangeEvent<HTMLTextAreaElement>) => {
+        setNewTask(event.target.value)
+    }
+
+    const handleSubmit = (event: FormEvent) => {
+        event.preventDefault()
+
+        onCreateNewTask(newTask)
+        setNewTask('')
+    }
+
     return (
-        <div className="">
-            <textarea />
-            <button type="submit" />
+        <div className={styles.container}>
+            <textarea onChange={handleNewTaskChange} placeholder="Add a new task" />
+            <button onSubmit={handleSubmit} type="submit">
+                Create
+                <PlusCircle size={16} weight="bold" />
+            </button>
         </div>
     )
 }
